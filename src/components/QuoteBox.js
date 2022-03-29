009380e209258be30c6b1b1ae79b0eb755ff8146
Jhoseb <jhoseb29@gmail.com>
2022-03-28 19:26:36 -0500

    solucionando error en netfly

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -17,7 +17,7 @@ const QuoteBox = ({quote}) => {
         
         <div>
          <button className='btn-twt'  title=''Share in twitter><a href="https://twitter.com/intent/tweet?text=Frases%20celebres%20para%20compartir%20en%20Twitter&hashtags=programación,html"
-          target="_blank"><i class="fa-brands fa-twitter"></i></a></button>   
+          ><i class="fa-brands fa-twitter"></i></a></button>   
         </div>
     </div>
     );
